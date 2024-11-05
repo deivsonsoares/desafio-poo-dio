92969cf90210d0d4827dadf46abc83e603e7ab4b
deivsonsoares <deivsonds.ds@gmail.com>
2024-11-05 04:47:03 -0300

    Implementados conceitos de Herança e polimorfismo
    Signed-off-by: deivsonsoares <deivsonds.ds@gmail.com>

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,28 +2,16 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
+
     private LocalDate data;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
     public Mentoria() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descrcao) {
-        this.descricao = descrcao;
     }
 
     public LocalDate getData() {
@@ -37,8 +25,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descrcao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descrcao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
